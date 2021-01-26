0a9157ae7dac48df09aaae4a0cea44f2af07d866
Molli V <mollitalkin@hotmail.com>
2021-01-25 21:52:47 -0800

    save button

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
 
 // Initial variables
+var eventText;
+var eventTime;
 
 //Displaying current date and time
     $("#currentDay").text(moment().format('MMMM Do YYYY'));
+
+//Save button functionality and to local storage
+    $(".saveBtn").click(function(){
+        eventText = $(this).siblings(".textarea").val();
+        eventTime = $(this).siblings(".textarea").val();
+        localStorage.setItem(eventTime, JSON.stringify(eventText));
+    })
 
 });
 
