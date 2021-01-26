3c2aaf3523d6d7a877be909ff633b94cacb434a8
Molli V <mollitalkin@hotmail.com>
2021-01-25 21:19:26 -0800

    display date

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,9 @@
-var DateTime = luxon.DateTime;
+$(document).ready(function() {
+
+// Initial variables
+
+//Displaying current date and time
+    $("#currentDay").text(moment().format('MMMM Do YYYY'));
+
+});
+
