f61a47e6db2322201690d80b6ad7857425deb396
Molli V <mollitalkin@hotmail.com>
2021-01-26 20:27:32 -0800

    pastpresentfuture

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,34 @@
 $(document).ready(function() {
 
-// Initial variables
-var eventText;
-var eventTime;
+// variables
+ var userText;
+ var eventTime;
+ var currentTime = moment().hour();
 
-//Displaying current date and time
-    $("#currentDay").text(moment().format('MMMM Do YYYY'));
+    //Displaying current date and time
+ $("#currentDay").text(moment().format('MMMM Do YYYY'));
 
-//Save button functionality and to local storage
-    $(".saveBtn").click(function(){
-        eventText = $(this).siblings(".textarea").val();
-        eventTime = $(this).siblings(".textarea").val();
-        localStorage.setItem(eventTime, JSON.stringify(eventText));
-    })
+    //function to change color of planner based on current time
+ $(".description").each(function() {
+    var scheduledTime = parseInt($(this).attr("id"));
+
+    if (scheduledTime < currentTime) {
+        $(this).addClass("past");
+    } else if (scheduledTime == currentTime) {
+        $(this).addClass("present");
+    } else if (scheduledTime > currentTime) {
+        $(this).addClass("future");
+    }
+ });
+
+    //Save button functionality and to local storage
+ $(".saveBtn").click(function(){
+    userText = $(this).siblings(".description").val();
+    eventTime = $(this).siblings(".hour").text();        localStorage.setItem(eventTime, JSON.stringify(userText));
+    console.log(eventTime);
+    console.log(userText);
+ });
+
 
 });
 
